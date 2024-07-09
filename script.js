6ac0b4072f907a5aa4a1d24a1583e661dd2ea687
hashimmalik59 <144908858+hashimmalik59@users.noreply.github.com>
2024-07-09 11:17:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function zero() {
+  document.querySelector("input").value = "0";
+}
+function one() {
+  document.querySelector("input").value = "1";
+}
+function two() {
+  document.querySelector("input").value = "2";
+}
+function three() {
+  document.querySelector("input").value = "3";
+}
+function four() {
+  document.querySelector("input").value = "4";
+}
+function five() {
+  document.querySelector("input").value = "5";
+}
+function six() {
+  document.querySelector("input").value = "6";
+}
+function seven() {
+  document.querySelector("input").value = "7";
+}
+function eight() {
+  document.querySelector("input").value = "8";
+}
+function nine() {
+  document.querySelector("input").value = "9";
+}
+function add() {
+  document.querySelector("input").value = "+";
+}
+function subtract() {
+  document.querySelector("input").value = "-";
+}
+function equal() {
+  document.querySelector("input").value = "=";
+}
+function multiply() {
+  document.querySelector("input").value = "*";
+}
+function divide() {
+  document.querySelector("input").value = "/";
+}
+function clear() {
+  document.querySelector("input").value = "/";
+}
+function clearAll() {
+  document.querySelector("input").value = "0";
+}
